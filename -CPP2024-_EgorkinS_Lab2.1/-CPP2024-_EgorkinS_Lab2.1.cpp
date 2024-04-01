04c8aa19aa8bf4e9b3899dc7d0b0e96748ab0df5
Mouser5 <stanislavegorkin7@gmail.com>
2024-04-01 22:14:17 +0300

    Удаление библиотеки и перенос все в класс
    Я неправильно подключил библиотеку и решил, что так будет надёжнее

--- -CPP2024-_EgorkinS_Lab2.1/-CPP2024-_EgorkinS_Lab2.1.cpp
+++ -CPP2024-_EgorkinS_Lab2.1/-CPP2024-_EgorkinS_Lab2.1.cpp
@@ -1,36 +1,107 @@
-﻿﻿#include <iostream>
-#include <vector>
+﻿#include <vector>
 #include <string>
-#include "Header.h"
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <iostream>
 using namespace std;
 
 class Student {
 private:
     string name;
-    string mark;
+    char mark;
     vector <double> ans;
 public:
-    Student(string _name, string _mark) {
+    Student(const string _name, const int number) {
         name = _name;
-        mark = _mark;
-        ans = Get_ans(mark);
+        Get_Mark(mark);
+        Get_ans(ans, mark, number);
     }
-    vector <double> Get_ans(string mark) {
+    void Get_Mark(char& mark) {
+        srand(time(0));
+        int score = rand() % (3) + 3;
+        if (score == 3)
+            mark = 'C';
+        if (score == 4)
+            mark = 'B';
+        else
+            mark = 'A';
+    }
+    void Get_ans(vector <double>& ans, const char mark, const int number) {
+        ans.resize(2);
+        if (mark == 'A') {
+            FindRoots(ans, number);
+        }
+        if (mark == 'C') {
+            ans[0] = 0.0;
+            ans[1] = NULL;
+        }
+        if (mark == 'B') {
+            int chance = rand() % (20) + 1;
+            if (chance <= 11)
+                FindRoots(ans, number);
+            else
+                ans[0] = 0.0;
+        }
+    } 
+    void InputData(double& a, double& b, double& c, const int number) {
+            ifstream file("input.txt");
+            string s;
+            for (int i = 1; i < number; i++) {
+                getline(file, s);
+            }
+            file >> a,b,c;
+     } 
+    void Roots(const double a, const double b, const double D, vector <double>& ans) {
+            ans[0] = (-b + sqrt(D)) / (2 * a);
+            ans[1] = (-b - sqrt(D)) / (2 * a);
+        };
+    void Discriminant(const double a, const double b, const double c, double& D) {
+            D = b * b - 4 * a * c;
+        }
+    void FindRoots(vector <double>& ans, const int number) {
+        double a, b, c, D;
+        InputData(a, b, c, number);
+        Discriminant(a, b, c, D);
+        if (D < 0) {
+            ans[0] = NULL;
+        }
+        else {
+            if (D == 0.0) {
+                ans[0] = (-b) / (2 * a);
+                ans[1] = NULL;
+            }
+            else
+                Roots(a, b, D, ans);
+        }
+    }
 
-        if (mark == "A") {
-
-
+    void PrintResult(const int number) {
+        cout << "Number:" << number << endl;
+        cout << name;
+        if (ans[0] == NULL) {
+            cout << "There are no roots.";
+        }
+        else {
+            cout << ans[0];
+            if (ans[1] != NULL) {
+                cout << ans[1];
+            }
         }
     }
 };
 
 int main()
-{
-    string s = "example";
-    Student* a = new Student(s, s);
-
-    // string s = "example";
-    // Student* a = new Student(s, s);
-    double a, b, c;
-    InputData(a, b, c);
+{    
+    //номер уравнения - number
+    // Фамилия - name
+    // Ответ - ans
+    string name;
+    int number;
+    cout << "Enter name...";
+    cin >> name;
+    cout << "Enter assignment's number...";
+    cin>> number;
+    Student* a = new Student(name, number);
+    a->PrintResult(number);
 }
--- -CPP2024-_EgorkinS_Lab2.1/Header.h
+++
@@ -1,3 +0,0 @@
-void Discriminant(const double a, const double b, const double c, double& D);
-void Roots(const double a, const double b, const double c, double D, double& x1, double& x2);
-void InputData(double& a, double& b, double& c);

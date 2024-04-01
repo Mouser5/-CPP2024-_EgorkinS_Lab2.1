1f22d2ca9428113d2170095cd3415add27ffc0cc
Mouser5 <stanislavegorkin7@gmail.com>
2024-04-01 22:20:18 +0300

    Создание цикла для проверки

--- -CPP2024-_EgorkinS_Lab2.1/-CPP2024-_EgorkinS_Lab2.1.cpp
+++ -CPP2024-_EgorkinS_Lab2.1/-CPP2024-_EgorkinS_Lab2.1.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+
 using namespace std;
 
 class Student {
@@ -22,86 +23,101 @@ public:
         int score = rand() % (3) + 3;
         if (score == 3)
             mark = 'C';
-        if (score == 4)
-            mark = 'B';
         else
-            mark = 'A';
+            if (score == 4)
+                mark = 'B';
+            else
+                mark = 'A';
     }
     void Get_ans(vector <double>& ans, const char mark, const int number) {
-        ans.resize(2);
         if (mark == 'A') {
             FindRoots(ans, number);
         }
         if (mark == 'C') {
-            ans[0] = 0.0;
-            ans[1] = NULL;
+            ans.resize(1);
+            ans[0] = 0;
+
         }
         if (mark == 'B') {
             int chance = rand() % (20) + 1;
             if (chance <= 11)
                 FindRoots(ans, number);
-            else
+            else {
+                ans.resize(1);
                 ans[0] = 0.0;
-        }
-    } 
-    void InputData(double& a, double& b, double& c, const int number) {
-            ifstream file("input.txt");
-            string s;
-            for (int i = 1; i < number; i++) {
-                getline(file, s);
             }
-            file >> a,b,c;
-     } 
-    void Roots(const double a, const double b, const double D, vector <double>& ans) {
-            ans[0] = (-b + sqrt(D)) / (2 * a);
-            ans[1] = (-b - sqrt(D)) / (2 * a);
-        };
-    void Discriminant(const double a, const double b, const double c, double& D) {
-            D = b * b - 4 * a * c;
+
         }
+    }
+    void InputData(double& a, double& b, double& c, const int number) {
+        ifstream file("input.txt");
+        string s;
+        for (int i = 1; i < number; i++) {
+            getline(file, s);
+        }
+        file >> a >> b >> c;
+    }
+    void Roots(const double a, const double b, const double D, vector <double>& ans) {
+        ans[0] = (-b + sqrt(D)) / (2 * a);
+        ans[1] = (-b - sqrt(D)) / (2 * a);
+    };
+    void Discriminant(const double a, const double b, const double c, double& D) {
+        D = b * b - 4 * a * c;
+    }
     void FindRoots(vector <double>& ans, const int number) {
         double a, b, c, D;
         InputData(a, b, c, number);
         Discriminant(a, b, c, D);
         if (D < 0) {
-            ans[0] = NULL;
+            ans.resize(0);
         }
         else {
             if (D == 0.0) {
+                ans.resize(1);
                 ans[0] = (-b) / (2 * a);
-                ans[1] = NULL;
             }
-            else
+            else {
+                ans.resize(2);
                 Roots(a, b, D, ans);
+            }
+
         }
     }
 
     void PrintResult(const int number) {
         cout << "Number:" << number << endl;
-        cout << name;
-        if (ans[0] == NULL) {
-            cout << "There are no roots.";
-        }
+        cout << name << endl;
+        int size = ans.size();
+        if (size == 0)
+            cout << "There are no roots." << endl;
         else {
-            cout << ans[0];
-            if (ans[1] != NULL) {
-                cout << ans[1];
+            for (int i = 0; i < size; i++) {
+                cout << ans[i] << endl;
             }
         }
     }
 };
 
-int main()
-{    
-    //номер уравнения - number
-    // Фамилия - name
-    // Ответ - ans
+void prog() {
     string name;
     int number;
     cout << "Enter name...";
     cin >> name;
     cout << "Enter assignment's number...";
-    cin>> number;
+    cin >> number;
     Student* a = new Student(name, number);
     a->PrintResult(number);
+}
+
+int main()
+{     
+    char another_attempt;
+    do
+    {
+
+        prog();
+        cout << "Do you want to try another student/number?...(y/n)";
+        cin >> another_attempt;
+    } while (another_attempt=='y');
+    return(0);
 }
